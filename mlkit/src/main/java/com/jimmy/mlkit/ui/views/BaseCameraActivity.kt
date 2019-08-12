cef7a490d5b7c5dd89801c259af55e9561ef0c92
Jamal Safwat <jamal.safwat@domain.itsoft.com>
2019-08-12 13:43:11 +0200

    - finalize google lens module before testing

--- mlkit/src/main/java/com/jimmy/mlkit/ui/views/BaseCameraActivity.kt
+++ mlkit/src/main/java/com/jimmy/mlkit/ui/views/BaseCameraActivity.kt
@@ -1,12 +1,15 @@
 package com.jimmy.mlkit.ui.views
 
 
-import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.annotation.LayoutRes
+import android.support.annotation.RequiresApi
 import android.support.design.widget.BottomSheetBehavior
 import android.support.design.widget.CoordinatorLayout
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
-import android.view.Gravity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_google_lens.*
 
@@ -15,9 +18,8 @@ abstract class BaseCameraActivity : AppCompatActivity(), View.OnClickListener {
 
     lateinit var sheetBehavior: BottomSheetBehavior<*>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    companion object{
+       const val CAMERA_REQUEST_CODE: Int= 1000
     }
 
     fun setupBottomSheet(@LayoutRes id : Int){
@@ -35,9 +37,22 @@ abstract class BaseCameraActivity : AppCompatActivity(), View.OnClickListener {
         sheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
             override fun onStateChanged(bottomSheet: View, newState: Int) {}
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
-                fab_take_photo.animate().scaleX(1 - slideOffset).scaleY(1 - slideOffset).setDuration(0).start()
+                fab_take_photo.animate().scaleX(1 - slideOffset)
+                                        .scaleY(1 - slideOffset)
+                                        .setDuration(0).start()
             }
         })
+    }
+
+
+     fun isCameraPermissionGranted(): Boolean {
+        val permission = ContextCompat.checkSelfPermission(this@BaseCameraActivity, Manifest.permission.CAMERA)
+        return permission == PackageManager.PERMISSION_GRANTED
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+     fun requestCameraPermission() {
+        requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
     }
 
 
--- mlkit/src/main/java/com/jimmy/mlkit/ui/views/googleLensClone/GoogleLens.kt
+++ mlkit/src/main/java/com/jimmy/mlkit/ui/views/googleLensClone/GoogleLens.kt
@@ -1,20 +1,131 @@
 package com.jimmy.mlkit.ui.views.googleLensClone
 
+
+import android.app.Activity
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.RequiresApi
+import android.support.design.widget.BottomSheetBehavior
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.Toast
+import com.jimmy.mlkit.BuildConfig
 import com.jimmy.mlkit.R
-
+import com.jimmy.mlkit.databinding.ActivityGoogleLensBinding
+import com.jimmy.mlkit.ui.views.BaseCameraActivity
+import com.mindorks.paracamera.Camera
 import kotlinx.android.synthetic.main.activity_google_lens.*
+import kotlinx.android.synthetic.main.image_label_layout.*
 
-class GoogleLens : AppCompatActivity() {
+class GoogleLens : BaseCameraActivity() {
+
+
+    private lateinit var itemAdapter: ImageLabelAdapter
+    private lateinit var camera: Camera
+
+
+    override fun onClick(v: View?) {
+       //
+    }
+
+    lateinit var binding : ActivityGoogleLensBinding
+    lateinit var mViewModel : GoogleLensViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_lens)
         setSupportActionBar(toolbar)
+        mViewModel = ViewModelProviders.of(this).get(GoogleLensViewModel::class.java)
+        fab_take_photo.setOnClickListener(this::takePicture)
+        setupBottomSheet(R.layout.image_label_layout)
+        rvLabel.layoutManager = LinearLayoutManager(this)
 
 
+        mViewModel.itemsFail.observe(this, Observer {
+
+            Toast.makeText(baseContext,"Sorry, something went wrong!", Toast.LENGTH_SHORT).show()
+
+        })
+
+        mViewModel.itemsSucces.observe(this, Observer {
+
+            itemAdapter = ImageLabelAdapter(it!!, false)
+            rvLabel.adapter = itemAdapter
+            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+        })
+
+        // initialize and configure the camera
+        camera = Camera.Builder()
+            .resetToCorrectOrientation(true)// Rotates the camera bitmap to the correct orientation from meta data.
+            .setTakePhotoRequestCode(Camera.REQUEST_TAKE_PHOTO)// Sets the request code for your onActivityResult() method
+            .setDirectory("pics")//Sets the directory in which your pictures will be saved.
+            .setName("delicious_${System.currentTimeMillis()}")// Sets the name of each picture taken according to the system time.
+            .setImageFormat(Camera.IMAGE_JPEG)//Sets the image format to JPEG
+            .setCompression(75)//Sets a compression rate of 75% to use less system resources
+            .build(this)
+    }
+
+
+
+    fun takePicture(view: View) {
+
+        if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(!isCameraPermissionGranted()){
+                requestCameraPermission()
+            }else{
+                try {
+                    camera.takePicture()
+                } catch (e: Exception) {
+                    // Show a toast for exception
+                    Toast.makeText(baseContext,"Sorry, something went wrong!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }else{
+            try {
+                camera.takePicture()
+            } catch (e: Exception) {
+                // Show a toast for exception
+                Toast.makeText(baseContext,"Sorry, something went wrong!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == Camera.REQUEST_TAKE_PHOTO) {
+                val bitmap = camera.cameraBitmap
+                if (bitmap != null) {
+                    imageView.setImageBitmap(bitmap)
+                    mViewModel.getLabelsFromDevice(bitmap)
+                } else {
+                    Toast.makeText(baseContext,"Sorry, something went wrong!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.contains(PackageManager.PERMISSION_GRANTED)) {
+                try {
+                    camera.takePicture()
+                } catch (e: Exception) {
+                    // Show a toast for exception
+                    Toast.makeText(baseContext,"Sorry, something went wrong!", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+               //
+            }
+        }
     }
 
 }
